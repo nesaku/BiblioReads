d6a58dfc6b3fc64f41e081a80c175eaa7a1bd528
Nesaku <78386413+nesaku@users.noreply.github.com>
2022-12-23 18:21:57 -0500

    add show/hide avatar toggle

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,7 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import ReadMore from "./ReadMore";
 
 const Reviews = (props) => {
+  const [showAvatars, setShowAvatars] = useState(false);
+  console.log(showAvatars);
   function Stars(props) {
     const starVal = props.starVal;
     if (starVal === "it was amazing") {
@@ -24,7 +26,23 @@ const Reviews = (props) => {
 
   return (
     <div id="bookReviews">
-      <h2 className="font-bold text-2xl my-8 underline">Reviews:</h2>
+      <h2 className="font-bold text-2xl mt-8 mb-4 underline">Reviews:</h2>
+      <div className="flex justify-center lg:justify-start items-center py-2">
+        <span class="mr-3 text-md font-medium text-gray-900 dark:text-gray-300">
+          Show Profile Avatars:
+        </span>
+        <label class="inline-flex relative items-center cursor-pointer">
+          <input
+            type="checkbox"
+            onClick={() => {
+              showAvatars ? setShowAvatars(false) : setShowAvatars(true);
+            }}
+            class="sr-only peer"
+          />
+          <div class="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-rose-300 dark:peer-focus:ring-rose-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-rose-600"></div>
+        </label>
+      </div>
+
       {props.data.map((data, i) => (
         <div
           id="review"
@@ -33,32 +51,34 @@ const Reviews = (props) => {
         >
           <div className="flex justify-between items-center mt-2 ml-6">
             <div className="flex items-center">
-              <div className="mr-2 w-12 h-12 overflow-hidden shadow rounded-full border-gray-500">
-                <picture>
-                  <source
-                    srcSet={`https://wsrv.nl/?url=${data.image}&default=${
-                      process.env.NEXT_PUBLIC_HOST_URL ||
-                      "http://localhost:3000"
-                    }/cover-placeholder.svg&output=webp&maxage=30d`}
-                    type="image/webp"
-                  />
-                  <source
-                    srcSet={`https://wsrv.nl/?url=${data.image}&default=${
-                      process.env.NEXT_PUBLIC_HOST_URL ||
-                      "http://localhost:3000"
-                    }/cover-placeholder.svg&maxage=30d`}
-                    type="image/jpeg"
-                  />
-                  <img
-                    src={`https://wsrv.nl/?url=${data.image}&default=${
-                      process.env.NEXT_PUBLIC_HOST_URL ||
-                      "http://localhost:3000"
-                    }/cover-placeholder.svg&maxage=30d`}
-                    alt="An image of the review author"
-                    loading="lazy"
-                  />
-                </picture>
-              </div>
+              {showAvatars && (
+                <div className="mr-2 w-12 h-12 overflow-hidden shadow rounded-full border-gray-500">
+                  <picture>
+                    <source
+                      srcSet={`https://wsrv.nl/?url=${data.image}&default=${
+                        process.env.NEXT_PUBLIC_HOST_URL ||
+                        "http://localhost:3000"
+                      }/cover-placeholder.svg&output=webp&maxage=30d`}
+                      type="image/webp"
+                    />
+                    <source
+                      srcSet={`https://wsrv.nl/?url=${data.image}&default=${
+                        process.env.NEXT_PUBLIC_HOST_URL ||
+                        "http://localhost:3000"
+                      }/cover-placeholder.svg&maxage=30d`}
+                      type="image/jpeg"
+                    />
+                    <img
+                      src={`https://wsrv.nl/?url=${data.image}&default=${
+                        process.env.NEXT_PUBLIC_HOST_URL ||
+                        "http://localhost:3000"
+                      }/cover-placeholder.svg&maxage=30d`}
+                      alt="An image of the review author"
+                      loading="lazy"
+                    />
+                  </picture>
+                </div>
+              )}
 
               <span className="text-gray-800 dark:text-gray-100 group">
                 <span className="text-md font-semibold">{data.author}</span>
