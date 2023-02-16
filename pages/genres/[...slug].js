7df8103464d994048d34716ca525320d725c986b
Nesaku <78386413+nesaku@users.noreply.github.com>
2023-02-16 16:47:08 -0500

    fix slug not being defined

--- pages/genres/[...slug].js
+++ pages/genres/[...slug].js
@@ -1,8 +1,12 @@
+import { useRouter } from "next/router";
 import Header from "../../components/global-components/Header";
 import Footer from "../../components/global-components/Footer";
 import UnsupportedRoute from "../../components/global-components/UnsupportedRoute";
 
 const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
   return (
     <div>
       <div className="bg-gradient-to-tr from-rose-50 to-rose-200 dark:bg-gradientedge text-gray-900 dark:text-gray-100 min-h-screen">
